bab48106db6eee2a165195c4ce4c5abfd21b06bf
heather7283 <heather7283@protonmail.com>
2024-09-14 22:51:50 +0400

    custom output format in cclip list

--- cclip.c
+++ cclip.c
@@ -3,10 +3,16 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "common.h"
 #include "config.h"
 #include "db.h"
+
+#define LIST_MAX_SELECTED_FIELDS 5
+const char* list_allowed_fields[LIST_MAX_SELECTED_FIELDS] = {
+    "rowid", "timestamp", "mime_type", "preview", "data_size"
+};
 
 int argc;
 char** argv;
@@ -14,18 +20,60 @@ char* prog_name;
 
 int print_row(void* data, int argc, char** argv, char** column_names) {
     UNUSED(data);
-    UNUSED(argc);
     UNUSED(column_names);
 
-    printf("%s\t%s\t%s\n", argv[0], argv[1], argv[2]);
+    for (int i = 0; i < argc; i++) {
+        printf("%s\t", argv[i]);
+    }
+    printf("\n");
     return 0;
 }
 
-void list(void) {
-    /* TODO: maybe allow user to specify which fields to output? */
+void list(char* fields) {
+    char* selected_fields[LIST_MAX_SELECTED_FIELDS];
+    int selected_fields_count = 0;
+
+    if (fields == NULL) {
+        selected_fields[0] = "rowid";
+        selected_fields[1] = "mime_type";
+        selected_fields[2] = "preview";
+        selected_fields_count = 3;
+    } else {
+        char* token = strtok(fields, ",");
+        while (token != NULL && selected_fields_count < LIST_MAX_SELECTED_FIELDS) {
+            bool is_valid_token = false;
+            for (int i = 0; i < LIST_MAX_SELECTED_FIELDS; i++) {
+                if (strcmp(token, list_allowed_fields[i]) == 0) {
+                    selected_fields[selected_fields_count] = token;
+                    selected_fields_count += 1;
+                    is_valid_token = true;
+                    break;
+                }
+            }
+
+            if (!is_valid_token) {
+                die("invalid field: %s\n", token);
+            }
+
+            token = strtok(NULL, ",");
+        }
+    }
+
+    if (selected_fields_count < 1) {
+        die("no fields selected\n");
+    }
+
     char* errmsg = NULL;
     int retcode;
-    const char* sql = "SELECT rowid,mime_type,preview FROM history ORDER BY timestamp DESC";
+
+    char sql[256] = "SELECT ";
+    for (int i = 0; i < selected_fields_count; i++) {
+        strcat(sql, selected_fields[i]);
+        if (i < selected_fields_count - 1) {
+            strcat(sql, ",");
+        }
+    }
+    strcat(sql, " FROM history ORDER BY timestamp DESC");
 
     retcode = sqlite3_exec(db, sql, print_row, NULL, &errmsg);
     if (retcode != SQLITE_OK) {
@@ -53,7 +101,7 @@ void get(int64_t id) {
 
         fwrite(data, 1, data_size, stdout);
     } else if (retcode == SQLITE_DONE) {
-        die("no entry found with specified id");
+        die("no entry found with specified id\n");
     } else {
         die("sqlite error: %s\n", sqlite3_errmsg(db));
     }
@@ -64,16 +112,22 @@ void print_help_and_exit(int exit_status) {
         "cclip - command line interface for cclipd database\n"
         "\n"
         "usage:\n"
-        "    %s list|get|delete|wipe|help [id]\n"
+        "    %s get|delete id\n"
+        "    %s list [rowid,timestamp,mime_type,preview,data_size]\n"
+        "    %s wipe\n"
+        "    %s help\n"
         "\n"
         "actions description:\n"
-        "    list       print list of all entries to stdout\n"
-        "    get        print entry with specified id to stdout\n"
-        "    delete     delete entry with specified id (not implemented)\n"
-        "    wipe       delete all entries from database (not implemented)\n"
-        "    help       display this help message\n";
+        "    list      print list of all saved entries to stdout\n"
+        "              up to %d comma-separated field names are accepted\n"
+        "    get       print entry with specified id to stdout\n"
+        "    delete    delete entry with specified id (not implemented)\n"
+        "    wipe      delete all entries from database (not implemented)\n"
+        "    help      display this help message\n";
 
-    fprintf(stderr, help_string, prog_name);
+    fprintf(stderr, help_string,
+            prog_name, prog_name, prog_name, prog_name,
+            LIST_MAX_SELECTED_FIELDS);
     exit(exit_status);
 }
 
@@ -90,7 +144,11 @@ int main(int _argc, char** _argv) {
     db_init(config.db_path);
 
     if (strcmp(argv[1], "list") == 0) {
-        list();
+        if (argc < 3) {
+            list(NULL);
+        } else {
+            list(argv[2]);
+        }
     } else if (strcmp(argv[1], "get") == 0) {
         if (argc < 3) {
             die("missing id\n");
