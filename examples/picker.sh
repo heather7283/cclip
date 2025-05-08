afc5add44fbea4928b6ea1123dbed133f1e1f46f
heather7283 <heather7283@protonmail.com>
2025-05-08 18:09:51 +0400

    examples: fix picker script

--- examples/picker.sh
+++ examples/picker.sh
@@ -15,7 +15,7 @@ fzf \
   --scheme history \
   --preview 'exec sh ./previewer.sh {}' \
   --preview-window wrap \
-  --bind "ctrl-d:execute-silent(cclip -s delete {1})+reload(${list_cmd})" \
+  --bind "ctrl-d:execute-silent(cclip delete -s {1})+reload(${list_cmd})" \
   --bind "ctrl-r:reload(${list_cmd})" \
   --bind "enter:become(cclip get {1} | wl-copy -t {2})"
 
